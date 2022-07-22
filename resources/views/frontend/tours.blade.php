0797f4b040cfeee83d14bf4e8c0319cff827c8a3
tunar036 <tunar036@gmail.com>
2022-07-22 17:14:38 +0400

    tours blade new changes

--- resources/views/frontend/tours.blade.php
+++ resources/views/frontend/tours.blade.php
@@ -18,13 +18,18 @@
             <div class="row">
                 <div class="col-md-12 text-center">
                     <div class="banner-content">
-                        <h2>{{__('lang.tours')}}</h2>
+                        @if (isset($category))
+                            <h2>{{ $category->getTranslatedAttribute('name', App::getLocale(), 'az') }}
+                                {{ __('lang.of_tours') }}</h2>
+                                @else
+                                <h2>{{ __('lang.tours') }}</h2>
+                        @endif
                         <nav aria-label="breadcrumb">
                             <ol class="breadcrumb">
                                 <li class="breadcrumb-item">
-                                    <a href="{{route('index')}}">{{__('lang.home')}} / </a>
+                                    <a href="{{ route('index') }}">{{ __('lang.home') }} / </a>
                                 </li>
-                                <li class="breadcrumb-item active" aria-current="page">{{__('lang.tours')}}</li>
+                                <li class="breadcrumb-item active" aria-current="page">{{ __('lang.tours') }}</li>
                             </ol>
                         </nav>
                     </div>
@@ -35,46 +40,31 @@
     <!-- end banner -->
     <!-- start popular tours -->
     <section class="section-spacing-tours tours">
-        <div class="container">
-            <div class="row align-items-center tour-bar-row">
-                <div class="col">
-                    {{-- <p class="tour-bar-text">Showing 1-6 of 20 results</p> --}}
-                    @if (!empty($category))
+        @if (count($tours) > 0)
+            <div class="container">
+                <div class="row align-items-center tour-bar-row">
+                    <div class="col">
+                        {{-- <p class="tour-bar-text">Showing 1-6 of 20 results</p> --}}
                         <h4>{{ $category->getTranslatedAttribute('name', App::getLocale(), 'az') }}
                             {{ __('lang.of_tours') }}</h4>
-                    @else
-                        <h4>{{ __('lang.tours') }}</h4>
+                    </div>
+                </div>
+                <div class="row" data-cues="slideInLeft">
+                    @foreach ($tours as $tour)
+                        @include('frontend.includes.tour')
+                    @endforeach
+                </div>
+                <div class="row">
+                    @if (count($tours) > 0)
+                        <div class="pagination-wrapper">
+                            {{ $tours->links() }}
+                        </div>
                     @endif
                 </div>
-                {{-- <div class="col d-flex justify-content-end">
-                    <div class="custom-select">
-                        <select>
-                            <option value="0">{{ __('lang.Sort') }}</option>
-                            <option value="1">Date</option>
-                            <option value="2">Rating</option>
-                        </select>
-                    </div>
-                    <a href="tours-list.html" class="tour-bar-icon">
-                        <i class="fas fa-bars"></i>
-                    </a>
-                    <a href="tours.html" class="tour-bar-icon">
-                        <i class="fas fa-th-large"></i>
-                    </a>
-                </div> --}}
             </div>
-            <div class="row" data-cues="slideInLeft">
-                @foreach ($tours as $tour)
-                    @include('frontend.includes.tour')
-                @endforeach
-            </div>
-            <div class="row">
-                @if ($tours->hasPages())
-                    <div class="pagination-wrapper">
-                        {{ $tours->links() }}
-                    </div>
-                @endif
-            </div>
-        </div>
+        @else
+            <h4>Bu istiqametde tur yoxdur</h4>
+        @endif
     </section>
 
     @include('frontend.includes.footer')
